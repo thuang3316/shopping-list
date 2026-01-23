fa98b35a5ce2c3b34ce0e31e23043d805f751b57
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2026-01-23 16:27:26 -0500

    add shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,32 @@
-import '../styling/styles.css'
-import { Nav } from './Nav'
+import { useEffect } from 'react'
+import { Card } from './Cards';
 
-export function Shop() {
+export function Shop({cards, setCards}) {
+
+    useEffect(() => {
+        if (cards.length === 0) {
+            const promises = [];
+            for (let i = 1; i <= 20; i++) {
+                promises.push(
+                    fetch(`https://fakestoreapi.com/products/${i}`)
+                        .then(res => res.json())
+                        .then(res => {return {...res, count: 0}})
+                );
+            }
+
+            Promise.all(promises)
+                .then(data => {
+                    setCards(data); 
+                })
+                .catch(err => console.error("Error fetching products:", err));
+        }  
+    }, [cards.length, setCards]);
+
     return (
         <>
-            <Nav />
-            <h1 className="page-header">Shop</h1>
+            <ul className="grid grid-cols-5 gap-5">
+                {cards.map(product => <Card product={product} setCards={setCards} />)}
+            </ul>
         </>
     )
 }
